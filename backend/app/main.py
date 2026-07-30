106aa0835eb7a99047bdfcc8b7cd04717be33fdc
Syntax-Programmer <anand6308anand@gmail.com>
2026-07-31 03:58:18 +0530

    fixed import

--- backend/app/main.py
+++ backend/app/main.py
@@ -3,7 +3,7 @@ import os
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
-from backend.app.api.router import api_router
+from app.api.router import api_router
 
 app = FastAPI(
     title="DocketIQ API",
